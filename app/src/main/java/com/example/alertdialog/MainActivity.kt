cdcc1266e74001ae04817bad35b81e5dcc2ec76b
faten.ghamdi <Faten.arg@hotmail.com>
2021-10-17 21:45:46 +0300

    Initial commit

--- app/src/main/java/com/example/alertdialog/MainActivity.kt
+++ app/src/main/java/com/example/alertdialog/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.alertdialog
 
+<<<<<<< HEAD
 import android.app.ProgressDialog
 import android.content.DialogInterface
+=======
+>>>>>>> a8f43b2 (Initial commit)
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock
+<<<<<<< HEAD
 import android.text.InputType
+=======
+>>>>>>> a8f43b2 (Initial commit)
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -14,6 +20,7 @@ import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnAlret: Button
+<<<<<<< HEAD
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -47,3 +54,54 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+=======
+    private lateinit var tvBtn:Button
+    private lateinit var goBtn:Button
+
+    private lateinit var edText:EditText
+    private lateinit var tvText: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btnAlret = findViewById(R.id.btnAlret)
+        btnAlret.setOnClickListener {
+
+            showAlert()
+        }
+    }
+
+
+
+
+    private fun showAlert() {
+        val dialogView = layoutInflater.inflate(R.layout.dailog, null)
+        val dialog = AlertDialog.Builder(this)
+        dialog.setView(dialogView)
+
+
+
+        edText = dialogView.findViewById(R.id.edInput)
+        tvText = dialogView.findViewById(R.id.tv2)
+        tvBtn = dialogView.findViewById(R.id.btnTv)
+        goBtn = dialogView.findViewById(R.id.btnGo)
+        tvBtn.setOnClickListener {
+            tvText.text = edText.text.toString()
+        }
+
+        goBtn.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+            intent.putExtra(AlarmClock.EXTRA_MESSAGE,"${edText.text}" )
+            startActivity(intent)
+        }
+
+        dialog.show()
+    }
+    }
+
+
+
+
+>>>>>>> a8f43b2 (Initial commit)
